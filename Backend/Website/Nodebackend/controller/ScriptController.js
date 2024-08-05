73aab8d1bbaf5d7f8369a0439681833561b4ef9f
sbarvaliya <sbarvaliya@audeering.com>
2024-08-05 22:51:10 +0200

    comment added

--- Backend/Website/Nodebackend/controller/ScriptController.js
+++ Backend/Website/Nodebackend/controller/ScriptController.js
@@ -1,7 +1,9 @@
 
+// Automatically download reports
+
 exports._startCollectingReports = async (req, res) => {
     try {
-            
+
         // var reports = await getReports();
 
         __.res(res, 'Successfully reports downloaded', 200)
